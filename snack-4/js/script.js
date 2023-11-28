5a8639912ef85d7fb0b4f4ac43fd8bf639221e08
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2023-11-28 13:21:19 +0100

    es-4

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -1,6 +1,14 @@
 
-function somma (...numeri, ...lettere){
-    num = 1, 2, 3;
-    lett = 'a', 'b', 'c';
+const numeri = [1, 2, 3, 4, 5];
+const lettere = ['a', 'b', 'c', 'd', 'e'];
+
+if (numeri.length !== lettere.length){
+    alert('non si puo fare')
+} else {
+    const newArray = [];
+
+    numeri.forEach(( element, index) => {
+        newArray.push(lettere[index], element);
+    })
+    console.log(newArray)
 }
-console.log(somma)
